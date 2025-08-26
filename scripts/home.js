28057a9a775fbff87119ded3b6392b7c81afb0ba
BlueSparkle2075 <blue.sparkling.cat@gmail.com>
2025-08-27 00:47:23 +0200

    again mdr

--- scripts/home.js
+++ scripts/home.js
@@ -8,7 +8,7 @@ async function sendFormData(data) {
     const response = await fetch(scriptURL, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'text/plain;charset=utf-8',
       },
       body: JSON.stringify(data),
     });
