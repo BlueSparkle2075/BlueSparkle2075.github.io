f87a35dfab96fec13e10e310eb3375bdd11bd149
BlueSparkle2075 <blue.sparkling.cat@gmail.com>
2025-08-26 23:59:17 +0200

    putain j'en ai marre

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbypezfUFRe0IQWvVi6Ruj4zvmDCn2NxX2WxDvH0g0tUr15KD-yhEjGTqEVIoBEGnV_jFA/exec'; // Vérifie que c'est la bonne URL !
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxT1bleE10nY8qZcnNdcxd7IY2MoUz39BouGzpEQcsH/dev'; // Vérifie que c'est la bonne URL !
 
 const responseMessage = document.getElementById('responseMessage');
 const form = document.getElementById('contactForm');
