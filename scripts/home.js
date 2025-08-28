5efc5558b9bbc7459b1c029222984c1d811a89e5
BlueSparkle2075 <blue.sparkling.cat@gmail.com>
2025-08-28 17:30:14 +0200

    test12312312312312312312312323

--- scripts/home.js
+++ scripts/home.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwBSh1zXHsk3pd1y-STfMqoOfI_c29zCm-kasd6Et5HgzWMIKax56iMaVwikW_C_WsgZw/exec'; // Vérifie que c'est la bonne URL !
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw2xnsELGx_Dx4MNx5hg1mW7m6bv-LJ-4HnG1AQPr6qTZy8lIDH6vmjyzERhme7jmgM8A/exec'; // Vérifie que c'est la bonne URL !
 
 const responseMessage = document.getElementById('responseMessage');
 const form = document.getElementById('contactForm');
@@ -21,25 +21,27 @@ async function sendFormData(data) {
       throw new Error(`Erreur HTTP : ${response.status}`);
     }
 
-    const result = await response.json();
-    if (result.response === 'registered with success') {
-      responseMessage.textContent = `Application received. Don't hope too much though. I'm lazy and it might take years before I read it.`;
-      form.reset();
-    } else if (result.response === 'already in data base') {
-      responseMessage.textContent = 'We got it already. Submitting more than once won’t make me less lazy.';
-      form.reset();
-    } else {
-      responseMessage.textContent = `Something wrong happened: ${result.message || 'Unknown error'}. Yes, you have to submit again. Or not.`;
-    }
+    responseMessage.textContent = `Application received. Don't hope too much though. I'm lazy and it might take years before I read it.`;
+  //   const result = await response.json();
+  //   if (result.response === 'registered with success') {
+  //     responseMessage.textContent = `Application received. Don't hope too much though. I'm lazy and it might take years before I read it.`;
+  //     form.reset();
+  //   } else if (result.response === 'already in data base') {
+  //     responseMessage.textContent = 'We got it already. Submitting more than once won’t make me less lazy.';
+  //     form.reset();
+  //   } else {
+  //     responseMessage.textContent = `Something wrong happened: ${result.message || 'Unknown error'}. Yes, you have to submit again. Or not.`;
+  //   }
   } catch (error) {
     console.error('Erreur lors de l’envoi :', error);
     responseMessage.textContent = `Erreur : ${error.message}. Try again, or not. Up to you.`;
   }
+  
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  const body = {
+  const data = {
     name: document.getElementById('name').value,
     gameId: document.getElementById('gameId').value,
     state: document.getElementById('state').value,
@@ -48,7 +50,7 @@ form.addEventListener('submit', (e) => {
     message: document.getElementById('message').value,
     action: 'spontCandidate',
   };
-  sendFormData(body);
+  sendFormData(data);
 });
 
 
